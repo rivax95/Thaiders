e70d822b992c0b8d15af454324e8c8ba86151c4f
Alex <alejandrotejemundos@hotmail.es>
2018-11-25 01:19:15 +0100

    FixDrop02 OptimizadoManager

--- ControladorFPS-Controller2/NetController/Assets/1 - Assets/Scripts/Controller/WeaponManager.cs
+++ ControladorFPS-Controller2/NetController/Assets/1 - Assets/Scripts/Controller/WeaponManager.cs
@@ -1,17 +1,19 @@
-﻿//                                          ▂ ▃ ▅ ▆ █ ZEN █ ▆ ▅ ▃ ▂ 
+﻿//                                          ▂ ▃ ▅ ▆ █ ARC █ ▆ ▅ ▃ ▂ 
 //                                        ..........<(+_+)>...........
-// .cs (//)
+// WeaponManager.cs (x/10/2018)
 //Autor: Alejandro Rivas                 alejandrotejemundos@hotmail.es
-//Desc:
-//Mod : 
-//Rev :
+//Desc: Gestiona todo el armamento
+//Mod : 0.4
+//Rev : -------------------------
 //..............................................................................................\\
+#region Librerias
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Alex.Controller;
 using System.Linq;
-
+#endregion
+#region DeclaracionEnums
 public enum ObjectInHands
 {
     fire,grenade,mele
@@ -26,8 +28,9 @@ public enum Weapon{
 }
 public enum Mele { }
 public enum Grenades {Bang,Flash,Toxic }
+#endregion
 public class WeaponManager : MonoBehaviour {
-   
+    #region Variables
     public static WeaponManager instance;
     public ObjectInHands Hands = ObjectInHands.fire;
     public Weapon currentWeapon = Weapon.Police9mm;
@@ -43,6 +46,8 @@ public class WeaponManager : MonoBehaviour {
     public GameObject dropPoint;
     public GameObject GetGunOBJ;
     public List<GameObject> Guns;
+    #endregion
+    #region Inicializadores
     void Awake()
     {
         if (instance == null)
@@ -53,12 +58,13 @@ public class WeaponManager : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+        transform.Find("FireWeapon").Find(weapons[CurrenWeaponIndex].ToString()).gameObject.SetActive(true);
+        WeaponbaseCurrent = transform.Find("FireWeapon").Find(weapons[CurrenWeaponIndex].ToString()).GetComponent<WeaponBase>();
+        ActualizarInventario();
     }
     void Start()
     {
-        transform.Find("FireWeapon").Find(weapons[CurrenWeaponIndex].ToString()).gameObject.SetActive(true);
-        WeaponbaseCurrent= transform.Find("FireWeapon").Find(weapons[CurrenWeaponIndex].ToString()).GetComponent<WeaponBase>();
-        ActualizarInventario();
+      
 
         foreach (WeaponBase item in WeaponsInInventory)
         {
@@ -70,6 +76,8 @@ public class WeaponManager : MonoBehaviour {
         }
 
     }
+    #endregion
+    #region Actualizadores
     void Update()
     {
         if (Input.GetKey(KeyCode.Alpha1))
@@ -83,13 +91,100 @@ public class WeaponManager : MonoBehaviour {
             {
                 CheckDrop();
             }
-        
-       
         if (GetGunOBJ)
         {
             CheckGetGun(GetGunOBJ);
         }
+        if (transform.Find("FireWeapon").childCount ==0 && Hands != ObjectInHands.grenade)
+        {
+            Hands = ObjectInHands.mele;
+            OnSwichHandWeapon(Hands);
+        }
     }
+    #endregion
+    #region Checkers
+    public void CheckGetGun(GameObject gun)
+    {
+
+        GetGun(gun);
+    }
+    void CheckDrop()
+    {
+        if (Hands != ObjectInHands.fire) return;
+        if (transform.Find("FireWeapon").childCount ==0) return;
+        WeaponbaseCurrent.OnSwich();
+        dropWeapon(WeaponbaseCurrent);
+        //ActualizarInventario();
+        //selecNextWeapon();
+
+    }
+    void CheckWeaponSwitch()
+    {
+        float mousewheel = Input.GetAxis("Mouse ScrollWheel");
+        if (WeaponbaseCurrent.fireLock) return;
+        if (Hands == ObjectInHands.grenade) return;
+        if (WeaponsInInventory.Count < 2) return;
+
+        if (mousewheel > 0)
+        {
+            SelectPreviousWeapon();
+        }
+        else if (mousewheel < 0)
+        {
+            selecNextWeapon();
+        }
+
+        Switch = true;
+    }
+    void checkAmountGrenades()
+    {
+
+    }
+    #endregion 
+    #region InventoryOperations //Solo operaciones Logicas
+    public void ActualizarInventario()
+    {
+        Debug.Log("Llamada");
+        for (int i = 0; i < WeaponsInInventory.Count; i++)
+        {
+            try
+            {
+                WeaponsInInventory[i].gameObject.SetActive(true);
+
+            }
+            catch { }
+        }
+
+        WeaponsInInventory.Clear();
+        foreach (Transform t in transform.Find("FireWeapon").transform)
+        {
+            WeaponsInInventory.Add(t.gameObject.GetComponent<WeaponBase>());
+        }
+
+
+        foreach (WeaponBase item in WeaponsInInventory)
+        {
+            item.gameObject.SetActive(false);
+            item.fireLock = false;
+        }
+        WeaponsInInventory[CurrenWeaponIndex].gameObject.SetActive(true);
+        WeaponbaseCurrent = WeaponsInInventory[CurrenWeaponIndex];
+    }
+    void ChangeGunPerIndex(int index)
+    {
+        WeaponbaseCurrent.OnSwich();
+        if (index <= WeaponsInInventory.Count)
+        {
+
+            foreach (WeaponBase item in WeaponsInInventory)
+            {
+                item.gameObject.SetActive(false);
+            }
+            CurrenWeaponIndex = index;
+            WeaponsInInventory[CurrenWeaponIndex].gameObject.SetActive(true);
+        }
+    }
+
     public void OnSwichHandWeapon(ObjectInHands type)
     {
         GameObject fire = transform.Find("FireWeapon").gameObject, grenade = transform.Find("GrenadeWeapon").gameObject, mele = transform.Find("MeleWeapon").gameObject;
@@ -99,7 +194,7 @@ public class WeaponManager : MonoBehaviour {
         {
             case ObjectInHands.fire:
                 fire.SetActive(true);
-                ActualizarInventario();
+                StartCoroutine(actuTemp());
                 break;
             case ObjectInHands.grenade:
                 grenade.SetActive(true);
@@ -111,117 +206,9 @@ public class WeaponManager : MonoBehaviour {
                 break;
         }
     }
-   public void CheckGetGun (GameObject gun)
-    {
-        
-        GetGun(gun);
-    }
-   public WeaponBase buscarArmaFuego(string name)
-   {
-       foreach (var item in WeaponsInInventory)
-       {
-           if (item.Name == name)
-           {
-               return item;
-           }
-       }
-       return null;
-   }
-    
-    public void GetGun(GameObject gun)
-    {
-        if (Input.GetKey(KeyCode.E))
-        {
-            Debug.Log("furula");
-            foreach (var item in Guns)
-            {
-                if(item.GetComponent<WeaponBase>().Name == gun.GetComponent<WeaponBase>().Name)
-                {
-                   GameObject obj= Instantiate(item, this.transform.Find("FireWeapon").transform);
-                    Copia( gun.GetComponent<WeaponBase>(),obj.GetComponent<WeaponBase>());
-                 
-                  //Destuir en networking sin mas
-                    if (buscarArmaFuego(gun.GetComponent<WeaponBase>().Name))
-                    {
-                        WeaponBase droped= buscarArmaFuego(gun.GetComponent<WeaponBase>().Name); //buscar esto en la lista
-
-                        ChangeGunPerIndex(WeaponsInInventory.IndexOf(droped)); //pasarle el indice de la de arriba
-                        dropWeapon(WeaponbaseCurrent);
-                    }
-                      Destroy(gun);
-                      ActualizarInventario();
-                      ChangeGunPerIndex(WeaponsInInventory.Count-1);
-                    break;
-                }
-            }
-        }
-
-    }
-    void CheckDrop()
-    {
-        if (Hands != ObjectInHands.fire) return;
-        WeaponbaseCurrent.OnSwich();
-        dropWeapon(WeaponbaseCurrent);
-     
-        selecNextWeapon();
-        
-    }
-    void SwitchModeWeapon()
-    {
-        if (Input.GetMouseButtonDown(2))
-        {
-            Debug.Log("Cambio");
-            Hands = (Hands==ObjectInHands.grenade) ? ObjectInHands.fire : ObjectInHands.grenade;
-            OnSwichHandWeapon(Hands);
-            //TODOOOOOOOOOOOOOOOOOOO
-            //2WeaponbaseCurrent.OnSwich();
-        }
-        if (Input.GetKey(KeyCode.V))
-        {
-            Hands = ObjectInHands.mele;
-            OnSwichHandWeapon(Hands);
-        }
-       
-    }
-    void SwitchToCurrentWeapon() // Conectar con el controller //Conectado por otro lado
-    {
-        Hands = ObjectInHands.fire;
-        OnSwichHandWeapon(Hands);
-        for (int i = 0; i < WeaponsInInventory.Count ; i++)
-        {
-            WeaponsInInventory[i].gameObject.SetActive(false);
-            WeaponsInInventory[i].isReloading = false;
-            WeaponsInInventory[i].fireLock = false;
-        }
-        //transform.Find(weapons[CurrenWeaponIndex].ToString()).gameObject.SetActive(true);
-        WeaponbaseCurrent.OnSwich();
-        WeaponsInInventory[CurrenWeaponIndex].gameObject.SetActive(true);
-        WeaponbaseCurrent = WeaponsInInventory[CurrenWeaponIndex].gameObject.GetComponent<WeaponBase>();
-        WeaponbaseCurrent.AsingConfigurations();
-        Switch = false;
-    }
-    void CheckWeaponSwitch()
-    {
-        float mousewheel = Input.GetAxis("Mouse ScrollWheel");
-        if (WeaponbaseCurrent.fireLock) return;
-        if (Hands == ObjectInHands.grenade) return;
-        if (WeaponsInInventory.Count < 2) return;
-      
-        if (mousewheel > 0)
-        {
-            SelectPreviousWeapon();
-        }
-        else if (mousewheel < 0)
-        {
-            selecNextWeapon();
-        }
-        
-        Switch = true;
-    }
-
     void SelectPreviousWeapon()
     {
-        
+
         if (CurrenWeaponIndex == 0)
         {
             CurrenWeaponIndex = WeaponsInInventory.Count - 1;
@@ -244,48 +231,119 @@ public class WeaponManager : MonoBehaviour {
         }
         SwitchToCurrentWeapon();
     }
-    void checkAmountGrenades()
+    void SwitchToCurrentWeapon() // Conectar con el controller //Conectado por otro lado
     {
-
-    }
-    void dropWeapon(WeaponBase current)
-    {
-        int i = WeaponsInInventory.IndexOf(WeaponbaseCurrent);
-        
-        dropIstantiate(current.gameObject);
-        WeaponsInInventory.RemoveAt(i);
-        Destroy(current.gameObject);
-        StartCoroutine(actuTemp());
-    }
-    void ChangeGunPerIndex(int index)
-    {
-        WeaponbaseCurrent.OnSwich();
-        if(index <= WeaponsInInventory.Count)
+        Hands = ObjectInHands.fire;
+        OnSwichHandWeapon(Hands);
+        for (int i = 0; i < WeaponsInInventory.Count; i++)
         {
-
-            foreach (WeaponBase item in WeaponsInInventory)
-            {
-                item.gameObject.SetActive(false);
-            }
-            CurrenWeaponIndex = index;
-           WeaponsInInventory[CurrenWeaponIndex].gameObject.SetActive(true);
+            WeaponsInInventory[i].gameObject.SetActive(false);
+            WeaponsInInventory[i].isReloading = false;
+            WeaponsInInventory[i].fireLock = false;
         }
+        //transform.Find(weapons[CurrenWeaponIndex].ToString()).gameObject.SetActive(true);
+        WeaponbaseCurrent.OnSwich();
+        WeaponsInInventory[CurrenWeaponIndex].gameObject.SetActive(true);
+        WeaponbaseCurrent = WeaponsInInventory[CurrenWeaponIndex].gameObject.GetComponent<WeaponBase>();
+        WeaponbaseCurrent.AsingConfigurations();
+        Switch = false;
+    }
+  
+    void Copia(WeaponBase orig, WeaponBase copia)
+    {
+        //copia.penetration = orig.penetration;
+        //copia.minpenetration = orig.minpenetration;
+        copia.RecoilAfect = orig.RecoilAfect;
+        copia.SpraySystem = orig.SpraySystem;
+        copia.Playeranim = orig.Playeranim;
+        copia.ShootPoint = orig.ShootPoint;
+        copia.DropedImg = orig.DropedImg;
+        copia.Model = orig.Model;
+        copia.Name = orig.Name;
+        copia.clipSize = orig.clipSize;
+        copia.bulletsLeft = orig.bulletsLeft;
+        copia.bulletsInClip = orig.bulletsInClip;
+        copia.maxAmmo = orig.maxAmmo;
+    }
+    #endregion
+    #region ReturnFunctions
+    public WeaponBase buscarArmaFuego(string name)
+   {
+       foreach (var item in WeaponsInInventory)
+       {
+           if (item.Name == name)
+           {
+               return item;
+           }
+       }
+       return null;
+   }
+
+    #endregion
+    #region Inputs
+    void SwitchModeWeapon()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Cambio");
+            Hands = (Hands==ObjectInHands.grenade) ? ObjectInHands.fire : ObjectInHands.grenade;
+            OnSwichHandWeapon(Hands);
+            //TODOOOOOOOOOOOOOOOOOOO
+            //2WeaponbaseCurrent.OnSwich();
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+            Hands = ObjectInHands.mele;
+            OnSwichHandWeapon(Hands);
+        }
+       
+    }
+    public void GetGun(GameObject gun)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Debug.Log("furula");
+            foreach (var item in Guns)
+            {
+                if (item.GetComponent<WeaponBase>().Name == gun.GetComponent<WeaponBase>().Name)
+                {
+                    GameObject obj = Instantiate(item, this.transform.Find("FireWeapon").transform);
+                    Copia(gun.GetComponent<WeaponBase>(), obj.GetComponent<WeaponBase>());
+
+                    //Destuir en networking sin mas
+                    if (buscarArmaFuego(gun.GetComponent<WeaponBase>().Name))
+                    {
+                        WeaponBase droped = buscarArmaFuego(gun.GetComponent<WeaponBase>().Name); //buscar esto en la lista
+
+                        ChangeGunPerIndex(WeaponsInInventory.IndexOf(droped)); //pasarle el indice de la de arriba
+                        dropWeapon(WeaponbaseCurrent);
+                    }
+                    Destroy(gun);
+                    ActualizarInventario();
+                    ChangeGunPerIndex(WeaponsInInventory.Count - 1);
+                    break;
+                }
+            }
+        }
+
     }
 
-  public  void dropIstantiate(GameObject current)
+    #endregion
+    #region Drop
+    public void dropIstantiate(GameObject current)
     {
         GameObject cube = Instantiate(current.GetComponent<WeaponBase>().Model);
 
-        cube.transform.position = new Vector3(dropPoint.transform.position.x, dropPoint.transform.position.y, dropPoint.transform.position.z+0.2f);
+        cube.transform.position = new Vector3(dropPoint.transform.position.x, dropPoint.transform.position.y, dropPoint.transform.position.z + 0.2f);
         //cube.transform.position = new Vector3(dropPoint.transform.position.x,dropPoint.transform.position.y,transform.position.z*-1);
         Rigidbody rig = cube.GetComponent<Rigidbody>();
         rig.mass = 2f;
         rig.drag = 2f;
-        rig.AddForce(dropPoint.transform.forward*15, ForceMode.Impulse);
+        rig.AddForce(dropPoint.transform.forward * 15, ForceMode.Impulse);
         cube.transform.Find("Canvas").gameObject.AddComponent<DropedGun>();
         //cube.transform.localScale = new Vector3(0.1f, 0.2f, 0.1f);
         // copiamos valores 
-       
+
         switch (current.GetComponent<WeaponBase>().Name)
         {
             case "Police9mm":
@@ -295,7 +353,7 @@ public class WeaponManager : MonoBehaviour {
             case "UMP45":
                 cube.AddComponent<SlideStopWeapon>().enabled = false; ;
                 Copia(current.GetComponent<WeaponBase>(), cube.GetComponent<SlideStopWeapon>());
-                
+
                 break;
             case "DefenderShotgun":
                 cube.AddComponent<Escopeta>().enabled = false;
@@ -306,7 +364,7 @@ public class WeaponManager : MonoBehaviour {
                 Copia(current.GetComponent<WeaponBase>(), cube.GetComponent<Sniper>());
                 break;
             default:
-                 cube.AddComponent<WeaponBase>().enabled = false; ;
+                cube.AddComponent<WeaponBase>().enabled = false; ;
                 Copia(current.GetComponent<WeaponBase>(), cube.GetComponent<WeaponBase>());
                 break;
         }
@@ -314,55 +372,24 @@ public class WeaponManager : MonoBehaviour {
         //añadimos RB
         cube.AddComponent<Rigidbody>();
         //fuerzas
-        cube.GetComponent<Rigidbody>().AddForce(transform.forward  *2, ForceMode.Impulse);
+        cube.GetComponent<Rigidbody>().AddForce(transform.forward * 2, ForceMode.Impulse);
     }
-    void Copia(WeaponBase orig,WeaponBase copia)
+    void dropWeapon(WeaponBase current)
     {
-        //copia.penetration = orig.penetration;
-        //copia.minpenetration = orig.minpenetration;
-        copia.RecoilAfect = orig.RecoilAfect;
-        copia.SpraySystem = orig.SpraySystem;
-        copia.Playeranim = orig.Playeranim;
-        copia.ShootPoint = orig.ShootPoint;
-        copia.DropedImg = orig.DropedImg;
-        copia.Model =orig.Model;
-        copia.Name = orig.Name;
-        copia.clipSize = orig.clipSize;
-        copia.bulletsLeft = orig.bulletsLeft;
-        copia.bulletsInClip = orig.bulletsInClip;
-        copia.maxAmmo = orig.maxAmmo;
-    }
-   public void ActualizarInventario()
-    {
-        for (int i = 0; i < WeaponsInInventory.Count; i++)
-        {
-              try
-            {
-               WeaponsInInventory[i].gameObject.SetActive(true);
-                 
-            }
-            catch { }
-        }
-      
-        WeaponsInInventory.Clear();
-        foreach (Transform t in transform.Find("FireWeapon").transform)
-        {
-            WeaponsInInventory.Add(t.gameObject.GetComponent<WeaponBase>());
-        }
-       
-      
-        foreach (WeaponBase item in WeaponsInInventory)
-        {
-            item.gameObject.SetActive(false);
-            item.fireLock = false;
-        }
-        WeaponsInInventory[CurrenWeaponIndex].gameObject.SetActive(true);
-        WeaponbaseCurrent = WeaponsInInventory[CurrenWeaponIndex];
-    }
+        int i = WeaponsInInventory.IndexOf(WeaponbaseCurrent);
 
-   IEnumerator actuTemp()
+        dropIstantiate(current.gameObject);
+        WeaponsInInventory.RemoveAt(i);
+        Destroy(current.gameObject);
+       
+        selecNextWeapon();
+    }
+    #endregion
+    #region corrutinas
+    IEnumerator actuTemp()
    {
        yield return new WaitForEndOfFrame();
        ActualizarInventario();
    }
+    #endregion
 }
