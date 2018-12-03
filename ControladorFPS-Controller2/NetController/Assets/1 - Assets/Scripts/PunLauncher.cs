38db09611681557062493df425ce875f78f43243
Alex <alejandrotejemundos@hotmail.es>
2018-12-03 15:05:58 +0100

    Welcome Photon

---
+++ ControladorFPS-Controller2/NetController/Assets/1 - Assets/Scripts/PunLauncher.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PunLauncher : MonoBehaviour {
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
